a0a8e9807fdbc0d19ee0337b34db26642c5ff4d6
Ernesto Dones Sierra <edones94@gmail.com>
2025-02-24 22:09:32 -0800

    added the is_staff required field for users

--- backend/users/models.py
+++ backend/users/models.py
@@ -28,6 +28,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         user = self.create_user(email, password, **extra_fields)
         user.is_admin = True
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
