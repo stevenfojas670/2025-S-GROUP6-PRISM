bfcfcd3667d9e25cc462d76a111a38a702676d1d
Steven Fojas <stevenfojas670@gmail.com>
2025-03-03 20:27:01 -0800

    Added authentication options

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,40 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import type { NextApiRequest, NextApiResponse } from "next"
 
-const handler = NextAuth({
-	// Auth Options go here
-	providers: [
-		GoogleProvider({
-			clientId: process.env.AUTH_GOOGLE_ID,
-			clientSecret: process.env.AUTH_GOOGLE_SECRET,
-		}),
-	],
-})
-
-export { handler as GET, handler as POST }
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+	return await NextAuth({
+		// Auth Options go here
+		providers: [
+			GoogleProvider({
+				clientId: process.env.AUTH_GOOGLE_ID,
+				clientSecret: process.env.AUTH_GOOGLE_SECRET,
+			}),
+		],
+		callbacks: {
+			async signIn({ user }) {
+				// Only allow users with unlv domains to sign in
+				if (
+					user.email?.includes("@unlv.edu") ||
+					user.email?.includes("@unlv.nevada.edu")
+				)
+					return true
+				return false
+			},
+			async redirect({ url, baseUrl }) {
+				// Return baseUrl for now
+				return baseUrl
+			},
+			async jwt({ token }) {
+				// Send to Django to be validated
+				return token
+			},
+			async session({ session, token }) {
+				// jwt() is called first and forwards the token here
+				// Whatever has been added to the token will appear here
+				// The session callback is called everytime the session is checked
+				return session
+			},
+		},
+	})
+}
