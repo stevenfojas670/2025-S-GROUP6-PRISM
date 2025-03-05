1cc873ea0d6bed056034db884881c87c9e996c53
levyd1 <dalevyy@gmail.com>
2025-03-05 21:41:43 +0000

    Created data ingestion file for CodeGrade submission data.

---
+++ backend/data_ingestion/extractStudentDataFromCodeGrade.py
@@ -0,0 +1,175 @@
+'''
+    Created by Daniel Levy, 2/21/2025
+
+    This script is a part of the Data Ingestion phase of PRISM.
+'''
+from zipfile import ZipFile
+import pandas as pd
+import json
+import os
+
+'''
+    Log error and continue on~~~~
+    Throw exception and report errors back to front end
+
+    Errors in json format 
+'''
+
+class CodeGradeData:
+    '''
+        Constructor will initialize this class when we are extracting
+        student data from CodeGrade
+    '''
+    def __init__(self, dirName):
+        self.dirName = dirName
+        self.className = self.section = self.semester = self.assignmentName = self.zipFileDirectory = ""
+        self.submissions = self.users = self.metaData = self.errors = []
+        self.extractStudentFilesFromZIP()
+
+    def extractStudentFilesFromZIP(self):
+        for file in os.listdir(self.dirName):
+            if file.endswith('.zip'):
+                self.parseZipFileName(file)
+                self.zipFileDirectory = f"{self.dirName}/{self.assignmentName}"
+
+                zipFile = ZipFile(f"{self.dirName}/{file}")
+                zipFile.extractall(self.zipFileDirectory)
+
+                # Make sure the ZIP file actually contains data.
+                if os.listdir(self.zipFileDirectory) == []:
+                    print(
+                        f"Error! The zip file for Assignment {self.assignmentName} did not contain any student submissions!")
+                    exit(1)
+                return
+
+        # If we reach this point, then no valid ZIP file was found. Once we integrate
+        # the CodeGrade API with this script, we don't need this error check anymore.
+        print("Error! No .zip file containing student submissions was found.")
+        exit(1)
+
+    '''
+        CodeGrade exports a ZIP file with the following title format:
+            '<CS Class> <Section> - <Semester> <Assignment Name>'
+        
+        This method will simply parse the title and save each part to 
+        the object's appropriate fields. No error handling is needed.
+    '''
+    def parseZipFileName(self, name):
+        zipFields = name.split('-', 2)
+
+        canvasName = zipFields[0].split(' ')
+        self.className = canvasName[0] + ' ' + canvasName[1]
+        self.section = canvasName[2]
+
+        self.semester = zipFields[1].strip()
+        self.assignmentName = zipFields[2][:-4].strip()
+
+    '''
+        Every exported CodeGrade ZIP file will contain a .cg-info.json file that keeps
+        track of all submissions and users. This is needed to ensure data authentication,
+        so it must be present in the file.
+    '''
+    def checkIfJSONFileExists(self):
+        if not os.path.exists(f"{self.zipFileDirectory}/.cg-info.json"):
+            print("Error! The extracted zip file is missing the '.cg-info.json' file.")
+            exit(1)
+
+    '''
+        Once the ZIP file has been extracted, we can now take the json 
+        data and populate the submissions/users fields.
+    '''
+    def extractJSON(self):
+        self.checkIfJSONFileExists()
+
+        cgInfo = open(f'{self.zipFileDirectory}/.cg-info.json', 'r')
+        jsonStudentData = json.load(cgInfo)
+
+        self.submissions, self.users = jsonStudentData['submission_ids'], jsonStudentData['user_ids']
+
+    '''
+        This method handles the extraction of CodeGrade metadata for
+        each student which is exported through a CSV file.
+    '''
+    def extractMetaDataFromCSV(self):
+        for file in os.listdir(self.dirName):
+            if file.endswith('.csv'):
+                csvFile = open(f"{self.dirName}/{file}", 'r')
+                df = pd.read_csv(csvFile)
+                csvFile.close()
+                self.metaData = df
+                return
+
+        print("No .csv file containing student metadata was found.")
+        exit(1)
+
+    '''
+        Here, we verify that every submission ID is linked back to a student. We check
+        the ZIP directory to make sure the submission is there and ensure the student 
+        name matches the name associated with the given submission.
+    '''
+    def verifyStudentSubmissionExists(self):
+        for key, value in self.submissions.items():
+            subID, studentName = self.checkIfStudentFileExists(key)
+
+            if subID != value:
+                self.addError(f"Submission ID #{subID} does not match for {studentName}.")
+
+    '''
+        Similarly, we check to make sure that each user ID in the metadata
+        corresponds to exactly one submission, and that submission matches
+        the appropriate student name.
+    '''
+    def verifyStudentUserExistsInMetaData(self):
+        for key, value in self.users.items():
+            subID, studentName = self.checkIfStudentFileExists(key)
+
+            entry = self.metaData.loc[self.metaData['Id'] == value]
+            entriesFound = len(entry)
+
+            if entriesFound < 1:
+                self.addError(f"Error! No metadata for user ID {value} was found.", 'meta')
+
+            if entriesFound > 1:
+                self.addError(f"Error! User ID {value} has more than one entry.", 'meta')
+
+            if entry.iloc[0, 2] != studentName:
+                self.addError(f"Error! User ID {value} does not have a matching name.", 'meta')
+
+    def checkIfStudentFileExists(self, fileName):
+        subID, studentName = fileName.split('-', 1)
+        studentName = studentName.strip()
+        subID = int(subID.strip())
+
+        if fileName not in os.listdir(self.zipFileDirectory):
+            self.addError(f"Submission for {studentName} is missing.",fileName)
+
+        return subID, studentName
+
+    def addError(self, msg, file):
+        self.errors.append(
+            {
+                'message': msg,
+                'file': file
+            }
+        )
+
+    def printDebugInfo(self):
+        print(self.submissions)
+        print(self.users)
+        print(self.metaData)
+
+def main():
+    cgData = CodeGradeData('cg_data')
+
+    cgData.extractJSON()
+    cgData.extractMetaDataFromCSV()
+    cgData.verifyStudentSubmissionExists()
+    cgData.verifyStudentUserExistsInMetaData()
+
+    cgData.printDebugInfo()
+
+    if (len(cgData.errors) > 0):
+        pass
+
+if __name__ == "__main__":
+    main()
