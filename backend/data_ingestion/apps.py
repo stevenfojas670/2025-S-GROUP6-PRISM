54cfde3dda92c662ce498909b580eeaa0916b7cc
levyd1 <dalevyy@gmail.com>
2025-03-19 07:15:13 +0000

    Added default files when creating 'data_ingestion' app with Django startapp command.

--- backend/data_ingestion/apps.py
+++ backend/data_ingestion/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AssignmentsConfig(AppConfig):
+class DataIngestionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'assignments'
+    name = 'data_ingestion'
