1cdd7a7ad7501136dbe68f4b670bf5d249e26361
Eli Rosales <ejerald.cr@gmail.com>
2025-03-20 04:24:21 -0700

    Revert "keep all the new files from PR"
    This reverts commit 383edce15810d68dea40ebc5044a8f6c9a041664, reversing
    changes made to b2fd137088661529fd26a9461de840305f8a76da.

---
+++ backend/courses/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from courses import models
+
+# Register your models here.
+admin.site.register(models.Professor)
+admin.site.register(models.Class)
+admin.site.register(models.Semester)
+admin.site.register(models.ProfessorClassSection)
