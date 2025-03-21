93d412c570bbfa84309e1a3c0b47db389f1b990a
jkasbohm <121525118+jkasbohm@users.noreply.github.com>
2025-03-21 13:57:50 -0700

    Update views.py

--- backend/prism_backend/views.py
+++ backend/prism_backend/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from google.oauth2 import id_token
@@ -31,7 +30,7 @@ def validate_token(self, request):
         return Response({"error": "Invalid token"}, status=401)
     except Exception as e:
         return Response({"error": str(e)}, status=500)
-=======
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -39,4 +38,3 @@ from rest_framework.response import Response
 def hello_world(request):
     return Response({"message": "Hello from Django Rest Framework!"})
 
->>>>>>> origin
