a0cda9b23088c2f9089371029d97bb05460f1e34
Evan Shumaker <evangshu@gmail.com>
2025-03-24 23:54:24 +0000

    Added an enrollments table. Meaning i changed models, serializers, urls, views, and admin.py

--- backend/courses/serializers.py
+++ backend/courses/serializers.py
@@ -60,3 +60,53 @@ class ProfessorClassSectionSerializer(serializers.ModelSerializer):
             'class_instance_id',
             'professor_id'
         ]
+
+
+# Serializer stuff lets us send data to frontend with API responses. 
+# Also validates incoming JSON data and helps by converting to an enrollment object when creating/updating records.
+# Similar structure to PCSSerializer
+class EnrollmentSerializer(serializers.ModelSerializer):
+    """Enrollment Model Serializer."""
+    
+    # For read operations, show full student/class/semester details. without this, the data thats returned for these would be just ids.
+    # This makes the data quickly readable for frontend.
+    # This works bc frontend probably just needs these names, and not the full detailes about each object. 
+    # It reduces db queries => improved performance
+    student = serializers.StringRelatedField(read_only=True)
+    class_instance = serializers.StringRelatedField(read_only=True)
+    semester = serializers.StringRelatedField(read_only=True)
+
+    # Just accepting ids for writes. Same reasoning as PCSSerializer. i.e. he client can just specify a student_id, instead
+    #of an entire student object. It also maps the student_id to the actual 'student' field in the enrollment (source='student')
+    student_id = serializers.PrimaryKeyRelatedField(
+        queryset=models.Student.objects.all(), 
+        source='student', 
+        write_only=True
+    )
+    class_instance_id = serializers.PrimaryKeyRelatedField(
+        queryset=models.Class.objects.all(), 
+        source='class_instance', 
+        write_only=True
+    )
+    semester_id = serializers.PrimaryKeyRelatedField(
+        queryset=models.Semester.objects.all(), 
+        source='semester', 
+        write_only=True
+    )
+
+    # This gives frontend requests full details of student, class, and semester (rather than just ids)
+    # I think the frontend can now just specify a student_id { "student_id": 1 }, instead of nesting a full student because of this.
+    class Meta:
+        model = models.Enrollment
+        fields = [
+            'id',
+            'student',
+            'student_id',
+            'class_instance',
+            'class_instance_id',
+            'semester',
+            'semester_id',
+            'enrolled_date',
+            'dropped',
+            'dropped_date'
+        ]
