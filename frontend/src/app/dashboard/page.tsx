759a462b05a91835fddfa408778384dc60b7505a
SendNudles <noodles1590@yahoo.com>
2025-03-24 20:16:39 -0700

    Dashboard
    # Summary
    updating the skeleton code and further laying out what needs to be done. nothing complex.
    
    # Description
    this is bare bones skeleton code. until we finalize the way we are linking pages that feature will be left unfinished. what is on the page are div containers and buttons for the compare students and alerts.
    
    # Documentation
    variables used for the page will be the following:
        Variables: Class_Name, Section_Name, Display_Name
        Buttons: Menu, Student Comparison, Alerts, n boxes with Class_Names and Section_Names
        Links: Student Comparison Tool, Alerts, Each box
        Header takes Display_Name
    
    documentation on how the page will function is found in the discord - additional documentation will be made at a later time. the wireframe is from Jacobs wireframe in the discord.
    
    # Unit Tests
    no unit tests were made are there is no functionality currently to test
    
    # PR Reviews
    front end should review and see if any changes or additions need to be made to the structure of the code.
    
    for what needs to be done:
    1. linking of the pages to the compare students and alerts pages
    2. creating and linking to the sections and their assignments
    3. creation of the banner and header for the welcome text
    4. further stylization and completion
    
    ![image_2025-03-24_201331187](https://github.com/user-attachments/assets/997355b9-ca34-41ae-92d9-71a04606fcdb)
    
    once linking and querey are added, tests will be made to ensure that the query is correct and that the changing of the pages is successful.

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,66 @@
+/* 
+    Variables: Class_Name, Section_Name, Display_Name
+    Buttons: Menu, Student Comparison, Alerts, n boxes with Class_Names and Section_Names
+    Links: Student Comparison Tool, Alerts, Each box
+    Header takes Display_Name
+*/
+
+import React from "react";
+
+/*
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import alerts from "./alerts";
+import compareStudents from "./compareStudents";
+*/
+
+// basic button layout -> need to acces how many sections a teacher has, their name and the class name -> create that many buttons/div containers to show
+// need an additional 2 buttons that are alwasy set for compare students and alerts -> respective buttons must be linked to their respective pages
+// an alert icon appears if a section has been detected of cheating
+
+// come back to header and replace the teacher name with the actual name assigned to the profile
+
+// one div for the creating the sections
+
+// the other div for the two main buttons at the top
+
+// waiting to see how the team is going to approach linking the pages
+
+function Dashboard() {
+    return (
+        <div>
+          <div>
+            <h1>"Hello, teacher name"</h1>
+          </div>
+            <div>
+              <button>Compare Students</button>
+              <button>Alerts</button>
+            </div>  
+          <div>
+              createSections();
+          </div>
+        </div>
+    );
+};
+
+export default Dashboard;
+
+// future code for when we begin to link the pages
+/*
+    <div>
+            <Link to="/studentComparison">
+                <button>Compare Students</button>
+            </Link>
+            <Link to="/about">
+                <button>Alerts</button>
+            </Link>
+        </div>
+            <div>
+                createSections();
+            </div>
+        </div>
+*/
+
+
+
