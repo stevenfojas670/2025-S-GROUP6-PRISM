91b4fc7448f2210b6de3998c5b93ac2ce18d2fdd
Steven Fojas <stevenfojas670@gmail.com>
2025-03-24 20:11:58 -0700

    Implemented user views

--- backend/users/views.py
+++ backend/users/views.py
@@ -9,6 +9,7 @@ Views for the User APIs.
 from users import models, serializers
 from courses.models import Professor
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.permissions import IsAdminUser
@@ -26,11 +27,34 @@ class UserVS(viewsets.ModelViewSet):
         "email",
     ]
 
-    # def get_queryset(self):
-    #     user = self.request.user
-    #     return models.User.objects.get(pk=user.pk)
+    def list(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["get"], url_path="me")
+    def me(self, request):
+        serializer = self.get_serializer(self.request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self):
+        pass
+
+    def update(self):
+        pass
+
+    def partial_update(self):
+        pass
+
+    def destroy(self):
+        pass
 
     def perform_create(self, serializer):
         """Create a user and automatically assign them as a Professor."""
         user = serializer.save()
         Professor.objects.create(user=user)
+
+    # def get_permissions(self):
+    #     if self.action in ["list", "create", "destroy"]:
+    #         return [IsAdminUser()]
+    #     return super().get_permissions()
