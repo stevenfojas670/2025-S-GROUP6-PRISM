801e46836528d184d3b89e09b614db26428bc8da
Ernesto Dones Sierra <edones94@gmail.com>
2025-03-26 20:04:55 -0700

    more actions

--- backend/prism_backend/settings.py
+++ backend/prism_backend/settings.py
@@ -90,11 +90,11 @@ ASGI_APPLICATION = 'prism_backend.asgi.application'  # Add this for WebSockets
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': config('DB_NAME', default='postgres'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='postgres'),
+        'HOST': config('DB_HOST', default='127.0.0.1'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
