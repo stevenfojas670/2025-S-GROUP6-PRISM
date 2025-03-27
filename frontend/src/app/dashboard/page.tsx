947a9540e7568547eb83201ec01be9cd1532d486
Steven Fojas <stevenfojas670@gmail.com>
2025-03-26 20:23:09 -0700

    Reverted dashboard back to Cassandra's changes

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,52 +1,66 @@
-"use client"
-import { Container, Typography, Button } from "@mui/material"
-import { SignOutButton } from "@/components/AuthenticationMethod"
-import { useCallback, useEffect, useState } from "react"
-import type { User } from "@/types/index"
-import { easyFetch } from "@/utils/fetchWrapper"
+/* 
+    Variables: Class_Name, Section_Name, Display_Name
+    Buttons: Menu, Student Comparison, Alerts, n boxes with Class_Names and Section_Names
+    Links: Student Comparison Tool, Alerts, Each box
+    Header takes Display_Name
+*/
 
-export default function Dashboard() {
-	const [users, setUsers] = useState<User[]>([])
-	const [loading, setLoading] = useState<Boolean>(false)
+import React from "react";
 
-	const fetchUsers = useCallback(async () => {
-		setLoading(true)
-		try {
-			const response = await easyFetch("http://localhost:8000/api/user/users", {
-				method: "get",
-			})
+/*
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-			const data = await response.json()
+import alerts from "./alerts";
+import compareStudents from "./compareStudents";
+*/
 
-			if (response.ok) {
-				setUsers(data)
-			} else {
-				console.error("Failed to fetch users.")
-			}
-		} catch (err) {
-			console.error(err)
-		} finally {
-			setLoading(false)
-		}
-	}, [])
+// basic button layout -> need to acces how many sections a teacher has, their name and the class name -> create that many buttons/div containers to show
+// need an additional 2 buttons that are alwasy set for compare students and alerts -> respective buttons must be linked to their respective pages
+// an alert icon appears if a section has been detected of cheating
 
-	return (
-		<Container>
-			<SignOutButton />
-			<Button onClick={fetchUsers}>
-				{loading ? "loading..." : "Fetch Users"}
-			</Button>
-			{users.length > 0 && (
-				<div>
-					{users.map((user, index) => (
-						<ul key={index}>
-							<li>{user.email}</li>
-							<li>{user.first_name}</li>
-							<li>{user.last_name}</li>
-						</ul>
-					))}
-				</div>
-			)}
-		</Container>
-	)
-}
+// come back to header and replace the teacher name with the actual name assigned to the profile
+
+// one div for the creating the sections
+
+// the other div for the two main buttons at the top
+
+// waiting to see how the team is going to approach linking the pages
+
+function Dashboard() {
+    return (
+        <div>
+          <div>
+            <h1>"Hello, teacher name"</h1>
+          </div>
+            <div>
+              <button>Compare Students</button>
+              <button>Alerts</button>
+            </div>  
+          <div>
+              createSections();
+          </div>
+        </div>
+    );
+};
+
+export default Dashboard;
+
+// future code for when we begin to link the pages
+/*
+    <div>
+            <Link to="/studentComparison">
+                <button>Compare Students</button>
+            </Link>
+            <Link to="/about">
+                <button>Alerts</button>
+            </Link>
+        </div>
+            <div>
+                createSections();
+            </div>
+        </div>
+*/
+
+
+
