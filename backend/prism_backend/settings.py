69e71b0ea530c186a8549afbc46a34c6b89e6088
Ernesto Dones Sierra <edones94@gmail.com>
2025-03-26 18:03:09 -0700

    sensitive info hiden

--- backend/prism_backend/settings.py
+++ backend/prism_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mq_ek!kc9dju!wwfbb+6syhy=cktdh$&m78lc*@jp#xpdse7%8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = ['10.238.2.238', 'localhost', '.10.', '.131.']
 ALLOWED_HOSTS = ['*']
@@ -89,15 +90,13 @@ ASGI_APPLICATION = 'prism_backend.asgi.application'  # Add this for WebSockets
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'prism_db',
-        #CHANGE TO THIS ONE I AM USING MY OWN DATABASE ON MY OWN LAPTOP
-        'USER': 'donessie',
-        'PASSWORD': 'Emily!2013',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
