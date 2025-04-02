6c3b5be0350837a3d22cee7bd6895c2b3be24e86
Ernesto Dones Sierra <edones94@gmail.com>
2025-04-01 23:38:49 -0700

    changed the cache backend to django-redis, and configurated the .yml to setup the enviroment on github actions

--- backend/prism_backend/settings.py
+++ backend/prism_backend/settings.py
@@ -165,7 +165,10 @@ REST_FRAMEWORK = {
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
-        "LOCATION": "127.0.0.1:11211",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
     }
 }
