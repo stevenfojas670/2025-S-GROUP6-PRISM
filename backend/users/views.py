48d0025c7ba83b3cfb60bd252f0673bc0f63592e
Steven Fojas <stevenfojas670@gmail.com>
2025-04-06 18:22:09 -0700

    Made changes to the actions to use google vars

--- backend/users/views.py
+++ backend/users/views.py
@@ -8,7 +8,6 @@ whenever needed.
 """
 
 from rest_framework import viewsets, status
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.request import Request
 
