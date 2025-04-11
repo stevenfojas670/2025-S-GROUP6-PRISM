7b260f52100dc152b66f5cbb896fa4de851ddd88
stevenfojas670 <84162024+stevenfojas670@users.noreply.github.com>
2025-04-10 23:53:19 -0700

    Created a layout change with a left panel and navbar. The layout will not appear on the login page. Pages will be rendered in the center. Left panel includes navigation for each semester and other links will switch pages. Created some controller functions for fetching multiple/single semesters, multiple courses, created some dummy data for for semesters, installed material ui icons, made adjustment to retrieving semesters in courses/views.py and moved components for student_comparison to singular componetns folder.

---
+++ frontend/src/components/RightPanel.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material"
+
+export default function RightPanel() {
+	return (
+		<Box
+			sx={{
+				width: 200,
+				minWidth: 150,
+				borderRadius: 2,
+				border: "1px solid white",
+				p: 1,
+			}}
+		>
+			Right Panel
+		</Box>
+	)
+}
