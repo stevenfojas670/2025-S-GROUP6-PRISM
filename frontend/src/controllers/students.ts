b24e134f89a2eeffe536b1a8503ab9f8309ff1d1
Bakera21 <bakera21@unlv.nevada.edu>
2025-04-18 11:18:47 -0700

    Renamed the type file for students, added in the controller for assignments
    I renamed the student.ts file that handled types to the more fitting studentTypes.ts, then implemented the assignments API controller.

--- frontend/src/controllers/students.ts
+++ frontend/src/controllers/students.ts
@@ -1,11 +1,11 @@
 import { easyFetch } from "@/utils/fetchWrapper"
-import { Student, Response, StudentResponse } from "@/types/student"
+import { Student, Response, StudentResponse } from "@/types/studentTypes"
 import { APIError } from "@/types/APIError"
 //need to get all students from specific course by course ID
 export async function GetStudents(id: number): Promise<StudentResponse | APIError> {
 	try {
 		const response = await easyFetch(
-			`http://localhost:8000/api/course/studentenrollments/?course_instance={id}}`,
+			`http://localhost:8000/api/course/studentenrollments/?course_instance=${id}`,
 			{ method: "GET" }
 		)
 
