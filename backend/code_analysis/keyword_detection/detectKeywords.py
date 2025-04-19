d1a7788467b059037ba69fce0b1d1cb3a0c6fcb2
levyd1 <dalevyy@gmail.com>
2025-04-19 17:16:02 +0000

    Created method to open and parse an input file containing banned keywords.

--- backend/code_analysis/keyword_detection/detectKeywords.py
+++ backend/code_analysis/keyword_detection/detectKeywords.py
@@ -1,21 +1,40 @@
+import sys
+import os
 
 class KeywordAnalyzer:
-    __inputFile = None
+    __assignmentNum = None
+    __words = None
 
     def __init__(self, inputFile):
+        self.__words = list()
         self.openAndValidateFile(inputFile)
 
+    '''
+        This method is responsible for opening the input file
+        containing the banned keywords, storing them into the
+        KeywordAnalyzer's words array, and also getting the 
+        assignment for which the keyword analysis will be done for
+    '''
     def openAndValidateFile(self,iFile):
-        try:
-            file = open(iFile,'r')
-        except:
-            print("ERROR!")
+        # ERROR CHECK #1: Make sure the file exists in the
+        #                 keyword_detection package
+        if not os.path.exists(iFile):
+            print("Error! Banned keyword file does not exist!")
             exit(1)
-        else:
-            __inputFile = file.readlines()
 
-def main():
-    analyzer = KeywordAnalyzer('banned_words.txt')
+        file = open(iFile,'r')
+
+        fileInput = file.readlines()
+        for w in fileInput:
+            self.__words.append(w.strip('\n'))
+
+        fileNameParts = iFile.split('_')
+        __assignmentNum = fileNameParts[0][2:]
+
+        file.close()
+
+def main(fileName):
+    analyzer = KeywordAnalyzer(fileName)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
