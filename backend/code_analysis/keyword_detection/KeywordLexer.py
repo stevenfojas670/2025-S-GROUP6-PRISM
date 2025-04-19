126998dad1f353945ee0ba7613a8cc8d79d23c7a
levyd1 <dalevyy@gmail.com>
2025-04-19 17:49:35 +0000

    Created initial helper methods to control state of KeywordLexer

--- backend/code_analysis/keyword_detection/KeywordLexer.py
+++ backend/code_analysis/keyword_detection/KeywordLexer.py
@@ -1,3 +1,5 @@
+import KeywordToken
+import KeywordTokenType
 
 class KeywordLexer:
     # Fields
@@ -10,3 +12,16 @@ class KeywordLexer:
         self.__program = file
         self.__lookahead = 0
         self.__lineNum = 1
+
+    def __consume(self):
+        if(self.__lookahead <= len(self.__program)):
+            self.__lookahead += 1
+
+    def __match(self,currChar):
+        if(currChar == self.__program[self.__lookahead]):
+            self.__consume()
+            return True
+        return False
+
+    def __nextToken(self):
+        pass
