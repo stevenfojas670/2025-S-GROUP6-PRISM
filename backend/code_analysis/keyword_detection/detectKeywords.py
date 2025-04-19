3da1d2f3f9fe7239c3b133d26c8b5ceea51d50ca
levyd1 <dalevyy@gmail.com>
2025-04-19 19:23:30 +0000

    Added support to store keyword usage inside a json file

--- backend/code_analysis/keyword_detection/detectKeywords.py
+++ backend/code_analysis/keyword_detection/detectKeywords.py
@@ -7,10 +7,12 @@ from clang.cindex import CursorKind
 class KeywordAnalyzer:
     __assignmentNum = None
     __words = None
+    __jsonFile = None
 
     def __init__(self, inputFile):
         self.__words = list()
         self.__openAndValidateFile(inputFile)
+        self.__createJSON()
         self.__runAnalysis()
 
     '''
@@ -37,18 +39,31 @@ class KeywordAnalyzer:
 
         file.close()
 
+    def __createJSON(self):
+        self.__jsonFile = open(f"{self.__assignmentNum}_found.json",'w')
+
     def __runAnalysis(self):
         dirName = f"/PRISM/data/assignments/assignment_{self.__assignmentNum}/bulk_submission"
 
         for f in os.listdir(dirName):
             if(not f.endswith(".csv")):
+                self.__jsonFile.write('{\n\t"errors": [\n')
                 self.__checkStudentFiles(f"{dirName}/{f}")
+                self.__jsonFile.write('\t]\n')
+                self.__jsonFile.write('}\n')
                 break
 
     def __checkStudentFiles(self,section):
         for f in os.listdir(section):
+            headerList = None
             if(not f.endswith(".json")):
                 program = cindex.Index.create()
+                # Parse program with all headers first
+                ast = program.parse(f"{section}/{f}/main.cpp",args=['-std=c++11'])
+                headerList = ast.get_includes()
+                for i in headerList:
+                    print(f"Header file: {i.include.name}")
+                print(headerList)
                 ast = program.parse(f"{section}/{f}/main.cpp",args=['-std=c++11','-nostdinc','-nostdlibinc'])
 
                 # Start from the root cursor
@@ -58,12 +73,11 @@ class KeywordAnalyzer:
     def __checkAST(self, currNode):
         for w in self.__words:
             if w == currNode.spelling:
-              #  if currNode.kind != CursorKind.VAR_DECL:
+              if currNode.kind != CursorKind.VAR_DECL or currNode.kind != CursorKind.DECL_REF_EXPR:
                 print(f"{currNode.spelling} was found at {currNode.location.line}")
 
         for c in currNode.get_children():
             self.__checkAST(c)
-
 
 def main(fileName):
     analyzer = KeywordAnalyzer(fileName)
