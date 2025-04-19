cd2bec0e9e5e4dd16edb5488e463a802d5442bb6
levyd1 <dalevyy@gmail.com>
2025-04-19 21:19:45 +0000

    Added missing comma

--- backend/prism_backend/settings.py
+++ backend/prism_backend/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'assignments',
     'drf_spectacular',
     'django_filters',
-    'code_analysis'
+    'code_analysis',
     'data_ingestion',
     'dj_rest_auth',
     'rest_framework.authtoken',
