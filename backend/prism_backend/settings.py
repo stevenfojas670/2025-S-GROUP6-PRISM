4d29facf3ca8b5a1c4ee188287f2c23194352000
Daniel Levy <128522767+Dalevyy@users.noreply.github.com>
2025-04-21 16:33:44 -0700

    Reverting change since it did not fix the merge conflict.

--- backend/prism_backend/settings.py
+++ backend/prism_backend/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "django_filters",
     "data_ingestion",
-    "code_analysis",
     "dj_rest_auth",
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
