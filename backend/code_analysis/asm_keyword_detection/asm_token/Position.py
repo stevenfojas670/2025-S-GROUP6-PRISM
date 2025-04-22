938ee660425e0528293fad70ce1c1823b529913c
levyd1 <dalevyy@gmail.com>
2025-04-22 02:28:51 +0000

    Created Position class to keep track of asm file positions.

---
+++ backend/code_analysis/asm_keyword_detection/asm_token/Position.py
@@ -0,0 +1,27 @@
+"""
+Created by Daniel Levy, 4/21/2025
+
+This is a simple class designed to represent a position. A position
+will be denoted by a line and a column, and we will use these positions
+to keep track of where each token is located in relation to the file.
+"""
+class Position:
+    __line = None   # Line Placement
+    __col = None    # Column Placement
+
+    def __init__(self):
+        self.__line = 0
+        self.__col = 0
+
+    def __init__(self,startLine,startCol):
+        self.__line = startLine
+        self.__col = startCol
+
+    def getLine(self):
+        return self.__line
+
+    def getCol(self):
+        return self.__col
+
+    def toString(self):
+        return f"<{self.__line}.{self.__col}>"
