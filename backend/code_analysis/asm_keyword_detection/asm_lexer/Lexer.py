b5d8dd47b11575c86942f9f1acf616990fcc1b8e
levyd1 <dalevyy@gmail.com>
2025-04-22 03:47:51 +0000

    Added helper methods to deal with whitespace and new line characters.

--- backend/code_analysis/asm_keyword_detection/asm_lexer/Lexer.py
+++ backend/code_analysis/asm_keyword_detection/asm_lexer/Lexer.py
@@ -2,9 +2,10 @@
 from code_analysis.asm_keyword_detection.asm_token import *
 
 class Lexer:
-    __EOF = '\0'
+    __EOF:str = '\0'
     __program:str = None
     __currPos:int = None
+    __lookahead:str = None
     __currLine:int = None
     __currCol:int = None
 
@@ -16,14 +17,30 @@ class Lexer:
         self.__currCol = 1
 
     def __consume(self):
-        if self.__curPos != self.__EOF:
+        if self.__lookahead != self.__EOF:
             self.__currPos += 1
+            self.__lookahead = self.__program[self.__currPos]
+            self.__curCol += 1
 
     def __match(self,currChar):
-        if(self.__program[self.__currPos] == currChar):
+        if(self.__lookahead  == currChar):
             return True
         return False
 
+    def __consumeNewLine(self):
+        self.__consume()
+        self.__currLine += 1
+        self.__currCol = 1
+
+    def __consumeWhiteSpace(self):
+        while self.__lookahead != [' ', '\t', '\r']:
+            self.__consume()
+
     def __nextToken(self):
-        while self.__program[self.__currPos] != self.__EOF:
+        while self.__lookahead != self.__EOF:
+            match self.__lookahead:
+                case '\n':
+                    self.__consumeNewLine()
+                case [' ','\t','\r']:
+                    self.__consumeWhiteSpace()
             pass
