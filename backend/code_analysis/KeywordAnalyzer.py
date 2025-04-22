5fb1f1bab51366b2383055d2e99e8b837230d325
levyd1 <dalevyy@gmail.com>
2025-04-22 18:56:35 +0000

    Refactored input validation method to have more error checks.

--- backend/code_analysis/KeywordAnalyzer.py
+++ backend/code_analysis/KeywordAnalyzer.py
@@ -4,34 +4,44 @@ import os
 
 class KeywordAnalyzer:
 
+    __words:set = None
+    __course:str = None
+    __assignment:str = None
+
     def __init__(self):
         self.__openAndValidateFile()
+        print(self.__course,self.__assignment,self.__words)
+
     '''
         This method is responsible for opening the input file
         containing the banned keywords, storing them into the
         KeywordAnalyzer's words array, and also getting the
         assignment for which the keyword analysis will be done for
     '''
-
     def __openAndValidateFile(self):
-        # ERROR CHECK #1: Make sure the file exists in the
+        # ERROR CHECK #1: Make sure the front end passed in the appropriate
+        #                 file arguments to this script
+        if len(sys.argv) != 2:
+            print("Usage: python3 KeywordAnalyzer.py <fileName>.txt")
+            exit(1)
+        elif not sys.argv[1].endswith(".txt"):
+            print("Error! A .txt file was not passed as an argument")
+            exit(1)
+
+        # ERROR CHECK #2: Make sure the file exists in the
         #                 keyword_detection package
         if not os.path.exists(sys.argv[1]):
             print("Error! Banned keyword file does not exist!")
             exit(1)
 
-        file = open(sys.argv[1], 'r')
+        fileParts = sys.argv[1].split("_")
+        self.__course = fileParts[0][2:]
+        self.__assignment = fileParts[1][2:]
 
-        fileInput = file.readlines()
-        for w in fileInput:
-            self.__words.append(w.strip('\n'))
-            self.__wordsCount.append(0)
-
-        fileNameParts = sys.argv[1].split('_')
-        self.__assignmentNum = fileNameParts[0][2:]
-
-        file.close()
-
+        with open(sys.argv[1], 'r') as iFile:
+            self.__words = set()
+            for l in iFile.readlines():
+                self.__words.add(l.strip('\n'))
 
 def main():
     KeywordAnalyzer()
