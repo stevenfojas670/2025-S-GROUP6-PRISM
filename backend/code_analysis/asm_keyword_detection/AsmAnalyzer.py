8ac91ba0cfacc41e4cf7426c60a912c12633e312
levyd1 <dalevyy@gmail.com>
2025-04-22 19:33:49 +0000

    Created helper method that will perform the assembly tokenization.

--- backend/code_analysis/asm_keyword_detection/AsmAnalyzer.py
+++ backend/code_analysis/asm_keyword_detection/AsmAnalyzer.py
@@ -1,13 +1,32 @@
 
 import os
-from asm_lexer import Lexer
+from code_analysis.asm_keyword_detection.asm_lexer.Lexer import Lexer
+from code_analysis.asm_keyword_detection.asm_token.Token import Token
+from code_analysis.asm_keyword_detection.asm_token.TokenType import TokenType
 
 class AsmAnalyzer:
 
     __words = None
     __oFile = None
+    __subDir = None
+    __tokens = None
 
-    def __init__(self,words,oFile):
+    def __init__(self, words, oFile, submissionDir):
         self.__words = words
         self.__oFile = oFile
+        self.__subDir = submissionDir
+        self.__tokenizeAssembly()
 
+    def __tokenizeAssembly(self):
+        for f in os.listdir(self.__subDir):
+            with open(f,'r') as submission:
+                lexer = Lexer(submission.readlines())
+                self.__tokens = list()
+                while True:
+                    currToken = lexer.nextToken()
+                    self.__tokens.append(currToken)
+
+                    if currToken.getType() == TokenType.EOF:
+                        break
+                    elif currToken.getType() == TokenType.ERROR:
+                        break
