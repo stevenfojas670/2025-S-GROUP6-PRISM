ba2a5a8334bced8f7f0b074319c8e1bda838c853
stevenfojas670 <84162024+stevenfojas670@users.noreply.github.com>
2025-04-22 20:27:42 -0700

    Made minor theme changes and dashboard course card styling

--- frontend/src/components/CourseCards.tsx
+++ frontend/src/components/CourseCards.tsx
@@ -1,31 +1,34 @@
 "use client"
-import {
-	Box,
-	Card,
-	CardHeader,
-	CardActions,
-	CardActionaArea,
-	CardContent,
-	Typography,
-} from "@mui/material"
+import { Box, Card, CardContent, Typography, ButtonBase } from "@mui/material"
+import { darken } from "@mui/material/styles"
 
 interface Props {
 	children: React.ReactNode
+	onClick?: () => void
 }
 
-export default function CourseCards({ children }: Props) {
+export default function CourseCards({ children, onClick }: Props) {
 	return (
-		<Box
-			sx={{
+		<ButtonBase
+			onClick={onClick}
+			sx={(theme) => ({
 				width: 300,
-				backgroundColor: "background.paper",
-				borderRadius: "5px",
-				justifyContent: "center",
-				display: "flex",
-				py: "10px",
-			}}
+				minHeight: 250,
+				borderRadius: "10px",
+				overflow: "hidden",
+				display: "block",
+				textAlign: "center",
+				px: 2,
+				py: 3,
+				boxShadow: 2,
+				transition: "background-color 0.3s ease",
+				backgroundColor: theme.palette.background.paper,
+				":hover": {
+					backgroundColor: darken(theme.palette.background.paper, 0.05),
+				},
+			})}
 		>
 			{children}
-		</Box>
+		</ButtonBase>
 	)
 }
