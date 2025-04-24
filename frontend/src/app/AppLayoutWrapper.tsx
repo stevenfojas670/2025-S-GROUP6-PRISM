f78754590d54b5f8bedc28bbfd63c5b8edb22483
stevenfojas670 <84162024+stevenfojas670@users.noreply.github.com>
2025-04-23 17:59:41 -0700

    Minor adjustment to AppLayoutWrapper

--- frontend/src/app/AppLayoutWrapper.tsx
+++ frontend/src/app/AppLayoutWrapper.tsx
@@ -4,7 +4,6 @@ import { useAuth } from "@/context/AuthContext"
 import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material"
 import { SignOutButton } from "@/components/AuthenticationMethod"
 import LeftPanel from "@/components/LeftPanel"
-import RightPanel from "@/components/RightPanel"
 
 interface Props {
 	children: React.ReactNode
@@ -12,51 +11,53 @@ interface Props {
 
 export default function AppLayoutWrapper({ children }: Props) {
 	const { user } = useAuth()
-
-	if (user === null) return children
-
-	return (
-		<Box
-			sx={{
-				display: "flex",
-				height: "100vh",
-				flexDirection: "column",
-			}}
-		>
-			<AppBar
-				position="static"
-				sx={(theme) => ({
-					backgroundColor: theme.palette.background.paper,
-					color: theme.palette.text.primary,
-				})}
-			>
-				<Toolbar>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-						PRISM
-					</Typography>
-					<Button variant="contained">
-						<SignOutButton />
-					</Button>
-				</Toolbar>
-			</AppBar>
+	console.log(user?.email)
+	if (user === null) {
+		return children
+	} else {
+		return (
 			<Box
 				sx={{
 					display: "flex",
-					height: "100%",
-					overflow: "hidden",
+					height: "100vh",
+					flexDirection: "column",
 				}}
 			>
-				<LeftPanel />
+				<AppBar
+					position="static"
+					sx={(theme) => ({
+						backgroundColor: theme.palette.background.paper,
+						color: theme.palette.text.primary,
+					})}
+				>
+					<Toolbar>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							PRISM
+						</Typography>
+						<Button variant="contained">
+							<SignOutButton />
+						</Button>
+					</Toolbar>
+				</AppBar>
 				<Box
 					sx={{
-						flexGrow: 1,
-						overflowY: "auto",
-						p: 2,
+						display: "flex",
+						height: "100%",
+						overflow: "hidden",
 					}}
 				>
-					{children}
+					<LeftPanel />
+					<Box
+						sx={{
+							flexGrow: 1,
+							overflowY: "auto",
+							p: 2,
+						}}
+					>
+						{children}
+					</Box>
 				</Box>
 			</Box>
-		</Box>
-	)
+		)
+	}
 }
