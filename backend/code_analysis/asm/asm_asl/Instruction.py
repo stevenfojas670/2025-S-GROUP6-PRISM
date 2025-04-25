c3b5353e1d21e2beef11fee9c2e705b505a3b567
levyd1 <dalevyy@gmail.com>
2025-04-25 20:12:35 +0000

    Added `Instruction` class.

---
+++ backend/code_analysis/asm/asm_asl/Instruction.py
@@ -0,0 +1,13 @@
+
+from code_analysis.asm.asm_asl.ASL import ASL
+
+class Instruction(ASL):
+    __instruction:str = None
+    __extraInfo = None
+
+    def __init__(self, instr, extra):
+        self.__instruction = instr
+        self.__extraInfo = extra
+
+    def toString(self):
+        return f"{self.__instruction}"
