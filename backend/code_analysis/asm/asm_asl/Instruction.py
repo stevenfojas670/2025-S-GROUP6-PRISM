5c651901e572a2d2fe83705d1cc3ab0ddcbf798b
levyd1 <dalevyy@gmail.com>
2025-04-25 20:41:16 +0000

    Updated `Instruction` class to correctly call super class code.

--- backend/code_analysis/asm/asm_asl/Instruction.py
+++ backend/code_analysis/asm/asm_asl/Instruction.py
@@ -5,7 +5,8 @@ class Instruction(ASL):
     __instruction:str = None
     __extraInfo = None
 
-    def __init__(self, instr, extra):
+    def __init__(self, startPos, endPos, instr, extra):
+        super().__init__(startPos, endPos)
         self.__instruction = instr
         self.__extraInfo = extra
 
