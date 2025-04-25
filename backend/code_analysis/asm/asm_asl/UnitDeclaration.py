ad5e1368930b5f1efab97613d7802aa625f8f23e
levyd1 <dalevyy@gmail.com>
2025-04-25 19:59:30 +0000

    Created class to handle `.bss` data declarations.

---
+++ backend/code_analysis/asm/asm_asl/UnitDeclaration.py
@@ -0,0 +1,14 @@
+
+from code_analysis.asm.asm_asl.ASL import ASL
+
+class UnitDeclaration(ASL):
+    __name:str = None
+    __size:str = None
+    __values = None
+
+    def __init__(self, startPos, endPos, name, size, values):
+        super().__init__(startPos, endPos)
+        self.__name = name
+        self.__size = size
+        self.__values = values
+
