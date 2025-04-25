e889b5cbd2419be4e75df9fdc5abd5f4e9ce6a12
levyd1 <dalevyy@gmail.com>
2025-04-25 18:11:06 +0000

    Wrote intitial parser methods for the first few rules.

--- backend/code_analysis/asm/asm_parser/Parser.py
+++ backend/code_analysis/asm/asm_parser/Parser.py
@@ -23,10 +23,30 @@ class Parser:
         else:
             return False
 
+    def __currentLA(self):
+        return self.__lookahead.getType()
+
     def __nextLA(self):
         if self.__currPos < len(self.__tokens) - 1:
             return self.__tokens[self.__currPos+1].getType()
         else:
             return None
 
+    # 1. <program> := <data>? <bss>? <text> ;
+    def program(self):
+        if self.__currentLA() == TokenType.SECTION and self.__nextLA() == TokenType.ID_DATA:
+            self.__data()
 
+    def __data(self):
+        self.__match(TokenType.SECTION)
+        self.__match(TokenType.ID_DATA)
+        while self.__currentLA() == TokenType.ID:
+            self.__decl()
+
+    def __decl(self):
+        self.__match(TokenType.ID)
+        if self.__currentLA == TokenType.EQU:
+            self.__match(TokenType.EQU)
+            self.__match(TokenType.NUM)
+        else:
+            # if self.__current
