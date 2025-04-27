eac3b2a23e4e7201009fa13c8b05b5883127fc7b
Evan Shumaker <evangshu@gmail.com>
2025-04-27 02:56:13 +0000

    Fixed some errors

--- backend/assignments/views.py
+++ backend/assignments/views.py
@@ -1,6 +1,5 @@
 """Assignment views with enhanced filtering, ordering, and search capabilities."""
 
-import pandas as pd
 from rest_framework import filters, viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,8 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from prism_backend.mixins import CachedViewMixin
 from users.permissions import IsProfessorOrAdmin
-from django.db.models import Max, Avg, F
-from assignments.models import SubmissionSimiliarityPairs #include this in the .models down below?
+from django.db.models import Count, Max, Avg, F
+from cheating.models import SubmissionSimiliarityPairs
 
 from .models import (
     Assignments,
@@ -155,23 +154,21 @@ class RequiredSubmissionFilesViewSet(viewsets.ModelViewSet, CachedViewMixin):
 class AggregatedAssignmentDataView(APIView):
     """
     API endpoint for aggregated assignment data.
-    
+
     Returns combined statistics (i.e. average grade) for submissions
     grouped by student and assignment. Professors, TAs, and admins can
     filter by students, assignments, and course.
     """
-    
+
     # just in case we add other perms...
     permission_classes = [IsAuthenticated, IsProfessorOrAdmin]
 
     def get(self, request, format=None):
+        """Returns aggregated similarity statistics for submissions."""
+
         student_ids = request.query_params.getlist("students")
         assignment_ids = request.query_params.getlist("assignments")
         course_id = request.query_params.get("course")
-
-        '''qs = Submissions.objects.select_related(
-            "student", "assignment", "course_instance", "course_instance__professor"
-        ).all()'''
 
         # Build a queryset that retrieves data along with similarity scores.
         qs = SubmissionSimiliarityPairs.objects.select_related(
@@ -179,10 +176,10 @@ class AggregatedAssignmentDataView(APIView):
             "submission_id_1__student",
             "submission_id_2"  # add other stuff if we want to show different things
         ).values(
-            "assignment__title",                           
-            "assignment__assignment_number",               
-            "submission_id_1__student__first_name",          
-            "submission_id_1__student__last_name",           
+            "assignment__title",
+            "assignment__assignment_number",
+            "submission_id_1__student__first_name",
+            "submission_id_1__student__last_name",
             "percentage"    # Similarity score
             # other fields here later...
         )
@@ -194,26 +191,17 @@ class AggregatedAssignmentDataView(APIView):
         if course_id:
             qs = qs.filter(course_instance_id=course_id)
 
-
-        #didnt want to use the dataframe method for this. Stuck to ORM aggregations
+        # didnt want to use the dataframe method for this. Stuck to ORM aggregations
         # qs becomes a list of dicts
-        '''data = list(qs)
-        if not data:
-            return Response({"message": "No data found for the specified filters."}, status=status.HTTP_404_NOT_FOUND)
-
-        df = pd.DataFrame.from_records(data)
-        # TODO: Check this is actually a date
-        df["created_at"] = pd.to_datetime(df["created_at"])'''
-
 
         # EVERYTHING gonna go in here ;)
         response_data = {}
 
-        '''Theres basically 2 strategies to filling response_data. 
+        '''Theres basically 2 strategies to filling response_data.
         Doing aggregations with .objects.values(...).annotate(...) (aka ORM aggregations)
         keeps aggregations in the db, meaning its faster bc of less data transferred on network. It only returns aggregated fields
         instead of full model instnaces or all raw data, this makes it fast too. Its simple for standard aggregations like
-        average and max. 
+        average and max.
         Disadvantages: More complex aggregations are a LOT harder. It can also be annoying to do multiple aggregations in the db.
         '''
 
@@ -225,7 +213,7 @@ class AggregatedAssignmentDataView(APIView):
         ).annotate(
             max_score=Max("percentage")
         ))
-        
+
         # Average sim score per assignment
         response_data["assignment_avg_similarity_score"] = list(SubmissionSimiliarityPairs.objects.values(
             "assignment__id",
@@ -233,9 +221,9 @@ class AggregatedAssignmentDataView(APIView):
         ).annotate(
             avg_score=Avg("percentage")
         ))
-        
+
         # Flagged submissions per assignment
-        #the 'F' lets each sim score be compared against its assignments own threshold. (dynamic)
+        # the 'F' lets each sim score be compared against its assignments own threshold. (dynamic)
         response_data["flagged_per_assignment"] = list(SubmissionSimiliarityPairs.objects.filter(
             percentage__gte=F('assignment__requiredsubmissionfile_set__similarity_threshold')
         ).values(
@@ -243,7 +231,7 @@ class AggregatedAssignmentDataView(APIView):
         ).annotate(
             flagged_count=Count("id")
         ))
-    
+
         # similarity score over time trend
         response_data["similarity_trends"] = list(
             Submissions.objects.values("created_at")
@@ -262,6 +250,5 @@ class AggregatedAssignmentDataView(APIView):
             SubmissionSimiliarityPairs.objects.values("assignment__course_instance__professor__user__username")
             .annotate(avg_similarity=Avg("percentage"))
         )
-        
-        # return Response(avgGradeGroup.to_dict(orient="records"), status=status.HTTP_200_OK)
+
         return Response(response_data, status=status.HTTP_200_OK)
